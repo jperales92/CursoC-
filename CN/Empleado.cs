3e3a325bea38b41f4ff6dd08005bcb0087a9935b
Jperales92 <jair.perales@gmail.com>
2018-04-28 13:12:36 -0600

    primer commit

---
+++ CN/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace CN
+{
+   public  class Empleado : Persona
+    {
+        public string usuario { get; set; }
+
+        public int idEmpleado { get; set; }
+        public Empleado(string _usuario,
+                        int _idEmpleado,
+                        string _nombreCompleto, 
+                        string _direccion,
+                        DateTime _fechaDenacimiento)
+            : base(_nombreCompleto, _direccion, _fechaDenacimiento)
+        {
+            this.usuario = _usuario;
+            this.idEmpleado = _idEmpleado;
+        }
+
+        public override string descripcion()
+        {
+            string resultado = string.Format("Usuario: {0} - ID Empleado: {1} - {2}",
+                                              usuario,idEmpleado,base.descripcion());
+            return resultado;
+        }
+
+    }
+}
---
+++ CN/Persona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CN
+{
+    public class Persona
+    {
+        #region Propiedades 
+
+        private string _nomreCompleto;
+        public string nombreCompleto
+        {
+            get { return _nomreCompleto; }
+            // set { _nomreCompleto = value; }
+        }
+
+        private DateTime _fechaNacimiento;
+        public DateTime fechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set { _fechaNacimiento = value; }
+        }
+
+        public int edad
+        {
+            get
+            {
+                DateTime hoy = DateTime.Today;
+                int edad = hoy.Year - fechaNacimiento.Year;
+                if (hoy < fechaNacimiento.AddYears(edad))
+                {
+                    edad--;
+                }
+                return edad;
+            }
+        }
+        public string Direccion { get; set; }
+        #endregion
+
+        #region Constructores
+
+        public  Persona(string _nombreCompleto, string _direccion, DateTime _fechaDenacimiento)
+        {
+            this._nomreCompleto = _nombreCompleto;
+            this.Direccion = _direccion;
+            this._fechaNacimiento = _fechaDenacimiento;
+        }
+
+        #endregion
+
+        #region Metodos
+        public virtual string descripcion()
+        {
+            string resultado = string.Format("Nombre: {0} - Dirrecion: {1} - Fecha Denacieminto :{2} - Edad : {3}",
+                                              nombreCompleto, Direccion, fechaNacimiento, edad);
+            return resultado;
+        }
+
+
+        #endregion
+
+    }
+}
